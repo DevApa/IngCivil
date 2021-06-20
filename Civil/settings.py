8a6eabe6a5079043ffdc2f2fba5f8044e3e32943
plataformaug <84280320+plataformaug@users.noreply.github.com>
2021-06-20 15:44:46 -0500

    creación de vistas de autenticación

--- Civil/settings.py
+++ Civil/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Authentication',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -103,15 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ec'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
